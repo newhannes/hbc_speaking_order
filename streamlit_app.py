c43b546ad76a8346b3fb1974183fcabbc4f36af3
Drew-WorkDesktop <drew.sikkink@mail.house.gov>
2024-05-02 12:08:35 -0400

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import pandas as pd
+import pdfkit
+
+st.title("House Budget Committee Speaking Order")
+
+data = pd.read_csv("budget_committee_members.csv")
+data["Member"] = data["Member"].str.split(" ")
+data["Member"] = data["Member"].apply(lambda x: x[1])
+
+members_present = st.multiselect("Select the members present:", data["Member"].tolist())
+st.session_state.members_present = members_present
+
+def speaking_order(present, data):
+    # Filter data for present members
+    data = data[data['Member'].isin(present)]
+    
+    # Sort Republicans and Democrats by rank
+    republicans = data[data['Party'] == 'R'].sort_values('Rank')
+    democrats = data[data['Party'] == 'D'].sort_values('Rank')
+    
+    # Interleave Republicans and Democrats
+    order = {"Speaker" : [], "Party" : [], "Rank":[], "State":[]}
+    for i in range(max(len(republicans), len(democrats))):
+        if i < len(republicans):
+            order["Speaker"].append(republicans.iloc[i]['Member'])
+            order["Party"].append(republicans.iloc[i]['Party'])
+            order["Rank"].append(republicans.iloc[i]['Rank'])
+            order["State"].append(republicans.iloc[i]['State'])
+        if i < len(democrats):
+            order["Speaker"].append(democrats.iloc[i]['Member'])
+            order["Party"].append(democrats.iloc[i]['Party'])
+            order["Rank"].append(democrats.iloc[i]['Rank'])
+            order["State"].append(democrats.iloc[i]['State'])
+    order = pd.DataFrame(order)
+    order["Order"] = order.index + 1
+    
+    return order
+
+subtle_red = "#ffcccc"
+subtle_blue = "#cce5ff"
+
+def highlight_party(row):
+     return [f'background-color: {subtle_red}']*len(row) if (row.Party == "R") else [f'background-color: {subtle_blue}']*len(row)
+def color_party(val):
+    color = subtle_red if val == "R" else subtle_blue
+    return f'background-color: {color}'
+
+if st.button("Generate Speaking Order"):
+    order = speaking_order(members_present, data)[["Order", "Speaker", "Party", "Rank", "State"]]
+    #order = order.set_index("Order")
+    styled = order.style.apply(highlight_party, axis=1)
+    html = styled.to_html(index=False)
+
+    st.write(html, unsafe_allow_html=True)
+
+    # Add a button to download the PDF
+    pdf = pdfkit.from_string(html, False, options={"enable-local-file-access": ""})
+    st.download_button(
+        "⬇️ Download PDF",
+        data=pdf,
+        file_name=f"House Budget Committee Speaking Order.pdf",
+    mime="application/octet-stream"
+)
+
+
+if st.button("Generate Speaking Order - Option 2"):
+    order = speaking_order(members_present, data)
+    order.set_index("Order", inplace=True)
+    st.table(order.style.apply(highlight_party, axis=1))

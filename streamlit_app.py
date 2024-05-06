81b5cce21ff98dc9e1485c0c4d46a32a4a7fdd84
Drew-WorkDesktop <drew.sikkink@mail.house.gov>
2024-05-06 12:22:32 -0400

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,16 +84,13 @@ if st.button("Generate Speaking Order"):
     # Use BeautifulSoup to Remove Party Column
     soup = BeautifulSoup(html_pdf, 'html.parser')
     table = soup.find('table')
-    first_row = table.find('tr')
-    new_tag = soup.new_tag('th')
-    new_tag.string = "Order"
-    first_row.insert(0, new_tag)   
+  
     for row in table.find_all('tr'):
         cells = row.find_all(["th", "td"])
         if len(cells) > 2:
             cells[3].extract()
          
-    html_pdf = str(soup)
+    html_pdf = str(soup.prettify("utf-8", "html"))
 
     # Add a button to download the PDF
     pdf = pdfkit.from_string(html_pdf, False, options={"enable-local-file-access": ""})
